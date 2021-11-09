a7cfa75d0c34f48591a55cbe684002284e1dbacc
Ryan <ryan.c.edge@outlook.com>
2021-11-09 02:04:46 -0500

    feat: add todo input field

--- chapter_3/start/lib/src/todo_vanilla/todo_view.dart
+++ chapter_3/start/lib/src/todo_vanilla/todo_view.dart
@@ -19,31 +19,84 @@ class TodoView extends StatelessWidget {
       ),
       body: Container(
         alignment: Alignment.topCenter,
+        margin: const EdgeInsets.symmetric(vertical: 30),
         child: ConstrainedBox(
           constraints: const BoxConstraints(maxWidth: 550),
-          child: AnimatedBuilder(
-            animation: controller,
-            builder: (BuildContext context, Widget? child) {
-              return ListView.builder(
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  final todo = controller.todos[index];
-                  return TodoListTile(
-                    todo: todo,
-                    onToggleComplete: (bool? value) {
-                      controller.update(todo.copyWith(complete: value));
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                child: TodoTextField(
+                  onSubmitted: (value) {
+                    controller.createTodo(Todo(
+                      id: controller.todos.length,
+                      task: value,
+                    ));
+                  },
+                ),
+              ),
+              AnimatedBuilder(
+                animation: controller,
+                builder: (BuildContext context, Widget? child) {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      final todo = controller.todos[index];
+                      return TodoListTile(
+                        todo: todo,
+                        onToggleComplete: (bool? value) {
+                          controller.update(todo.copyWith(complete: value));
+                        },
+                        onDelete: () {
+                          controller.deleteTodo(todo);
+                        },
+                      );
                     },
-                    onDelete: () {
-                      controller.deleteTodo(todo);
-                    },
+                    itemCount: controller.todos.length,
                   );
                 },
-                itemCount: controller.todos.length,
-              );
-            },
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+}
+
+class TodoTextField extends StatefulWidget {
+  const TodoTextField({
+    Key? key,
+    required this.onSubmitted,
+  }) : super(key: key);
+  final Function(String value) onSubmitted;
+  @override
+  _TodoTextFieldState createState() => _TodoTextFieldState();
+}
+
+class _TodoTextFieldState extends State<TodoTextField> {
+  late final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _controller,
+      textInputAction: TextInputAction.done,
+      decoration: const InputDecoration.collapsed(
+        hintText: 'What needs to be done?',
+      ),
+      onSubmitted: (value) async {
+        widget.onSubmitted(value);
+
+        _controller.clear();
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
