1590791c4b0f0ce09d0d5c70f943506fe194fd8a
Alberto <betoman96@gmail.com>
2021-12-23 17:59:53 +0100

    chore: removed unused field

--- chapter_9/test/mock_wrapper.dart
+++ chapter_9/test/mock_wrapper.dart
@@ -5,21 +5,15 @@ class MockWrapper extends StatelessWidget {
   /// The child to be tested.
   final Widget child;
 
-  /// This is useful when there's the need to make sure that a route is pushed
-  /// or popped.
-  final List<NavigatorObserver> navigatorObservers;
-
   /// The [child] to be tested.
   const MockWrapper({
-    required this.child,
     Key? key,
-    this.navigatorObservers = const [],
+    required this.child,    
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorObservers: navigatorObservers,
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: child,
