07cce059d231a505544f9e657feaaab22fea358a
Ryan Edge <ryan.c.edge@outlook.com>
2022-05-08 19:57:23 -0400

    feat: chapter 7 step 1

--- chapter_7/step_1/lib/src/ui/excuse/excuses_view.dart
+++ chapter_7/step_1/lib/src/ui/excuse/excuses_view.dart
@@ -41,8 +41,49 @@ class _ExcusesViewContent extends StatefulWidget {
   State<_ExcusesViewContent> createState() => _ExcusesViewContentState();
 }
 
-class _ExcusesViewContentState extends State<_ExcusesViewContent> {
+class _ExcusesViewContentState extends State<_ExcusesViewContent>
+    with TickerProviderStateMixin {
   int currentPage = 0;
+
+  late Animation<double> _loadingAnimation;
+  late AnimationController _loadingAnimationController;
+  late Animation<double> _excusesAnimation;
+  late AnimationController _excusesAnimationController;
+
+  @override
+  void initState() {
+    _loadingAnimationController = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    _loadingAnimation =
+        Tween<double>(begin: 1, end: 0).animate(_loadingAnimationController);
+    _excusesAnimationController = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    _excusesAnimation =
+        Tween<double>(begin: 0, end: 1).animate(_excusesAnimationController);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant _ExcusesViewContent oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.excuses != null) {
+      _loadingAnimationController
+          .forward()
+          .then((value) => _excusesAnimationController.forward());
+    } else {
+      _excusesAnimationController
+          .reverse()
+          .then((value) => _loadingAnimationController.reverse());
+    }
+  }
+
+  @override
+  void dispose() {
+    _loadingAnimationController.dispose();
+    _excusesAnimationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,14 +93,36 @@ class _ExcusesViewContentState extends State<_ExcusesViewContent> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: widget.excuses == null
-              ? const Center(
+          child: Stack(
+            children: [
+              AnimatedBuilder(
+                animation: _loadingAnimation,
+                builder: (context, child) {
+                  return Opacity(
+                    opacity: _loadingAnimation.value,
+                    child: child,
+                  );
+                },
+                child: const Center(
                   child: CircularProgressIndicator(),
-                )
-              : ExcusePageView(
-                  excuses: widget.excuses!,
-                  currentExcuse: currentPage,
                 ),
+              ),
+              if (widget.excuses != null)
+                AnimatedBuilder(
+                  animation: _excusesAnimation,
+                  builder: (context, child) {
+                    return Opacity(
+                      opacity: _excusesAnimation.value,
+                      child: child,
+                    );
+                  },
+                  child: ExcusePageView(
+                    excuses: widget.excuses!,
+                    currentExcuse: currentPage,
+                  ),
+                )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
